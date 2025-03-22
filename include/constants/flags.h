ccd5103dfa09ed983b1a013e5ac91190d71dc8e4
IlliterateJam <bennewhouse@vt.edu>
2025-03-22 17:07:03 -0400

    Fallarbor Ability Aficionado

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1505,7 +1505,7 @@
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
 #define FLAG_HIDE_BERRY_SELLER_GIRL                 (SYSTEM_FLAGS + 0x85) 
-#define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
+#define FLAG_GAVE_HEART_SCALES                      (SYSTEM_FLAGS + 0x86) 
 #define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
 #define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
