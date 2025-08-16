4f7c89bf6e531a07c7db397034141d2faaae5657
IlliterateJam <bennewhouse@vt.edu>
2025-08-16 15:01:34 -0400

    Fossils Moved and Buried

--- src/data/script_menu.h
+++ src/data/script_menu.h
@@ -619,6 +619,9 @@ static const struct MenuAction MultichoiceList_Fossil[] =
 {
     {gText_ClawFossil},
     {gText_RootFossil},
+    {gText_HelixFossil},
+    {gText_DomeFossil},
+    {gText_AmberFossil},
     {gText_Exit},
 };
 
