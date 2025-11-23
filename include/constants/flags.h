89c9853b98e11c1984ef0047097047c3c1e6c48c
IlliterateJam <bennewhouse@vt.edu>
2025-11-23 16:05:48 -0500

    Hints at Pacidlog

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1270,7 +1270,7 @@
 #define FLAG_DEFEATED_REGIGIGAS                                         0x4C4 // Unused Flag
 #define FLAG_SYS_BRAILLE_REGIELEKI_COMPLETED                                          0x4C5 // Unused Flag
 #define FLAG_SYS_BRAILLE_REGIDRAGO_COMPLETED                                                0x4C6 // Unused Flag
-#define FLAG_UNUSED_0x4C7                                           0x4C7 // Unused Flag
+#define FLAG_SAVED_HOENN                                          0x4C7 // Unused Flag
 #define FLAG_UNUSED_0x4C8                                           0x4C8 // Unused Flag
 #define FLAG_UNUSED_0x4C9                                           0x4C9 // Unused Flag
 #define FLAG_UNUSED_0x4CA                                           0x4CA // Unused Flag
